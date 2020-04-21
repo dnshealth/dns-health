1e11ee715d14d2459de9dbf304e2127a4758a573
Byon2 <44954476+Byon2@users.noreply.github.com>
2020-04-21 14:56:17 +0200

    Update dns_test_recursion.py

--- dns_test_recursion.py
+++ dns_test_recursion.py
@@ -1,13 +1,14 @@
 def check_recursive(q, ns_list):
     # checks for if RD flag is checked in the response
+    # q is for the server outside the jurisdiction of the name servers
+    # ns_list is a list of all the name servers to be tested
     import dns.message
     import dns.query
     import socket
     recursion_exists = False
     for x in ns_list:
         query = dns.message.make_query(q, dns.rdatatype.NS)
-        x = socket.gethostbyname(x)
-        x = "192.168.1.1"
+        x = socket.gethostbyname(x) 
         response = dns.query.udp(query, x)
         s = str(response)
         if "RA" in s: # When "RA" is in the response message then the ns server tells the client that recursion have happened
