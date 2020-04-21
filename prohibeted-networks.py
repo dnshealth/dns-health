fb80ceb91853e5d3b777a8c0fac57288f22b18ae
Hristo Georiev <hristog@kth.se>
2020-04-21 11:03:18 -0400

    Prohibited network check minor bugs

--- prohibeted-networks.py
+++ prohibeted-networks.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
 
-import dns.resolver
+# This feature will check if the NS server IP is global or private
+# If it is global it will pass the test
+# If it is local it will prompt the user that this IP is from a private network and
+# test will fail
 
-domain = "codeitfactory.com"
-result = dns.resolver.query(domain, 'A')
-for ipval in result:
-    print('IP', ipval.to_text())
+# Variables:
+# ns_server --> should be taken as an argument from the command line
+# ip_val --> extracted value of the ns server
+import dns.resolver
+from IPy import IP
+import ipaddress
+
+
+def prohibited_chech(ns_server):
+    result = dns.resolver.query(ns_server, 'A')
+    for ipval in result:
+
+        if ipaddress.ip_address(str(ipval)).is_private:
+            print("[+] Error ! The IP is not a global IP.")
+            print("[+] TEST FAIL")
+
+        else:
+            print("[+] The IP for the for " + ns_server + " is: " + ipval.to_text())
+            print("[OK]")
+
+
+# Hard code check
+prohibited_chech("ns1.loopia.se")
