64be432f1e49e7fdbbc1288ac6069702e464c5cf
Byon <bagren@kth.se>
2020-04-21 08:38:12 +0200

    commited

--- minimal_ns.py
+++ minimal_ns.py
@@ -1,8 +1,8 @@
 import socket
 
-def unique_ns_list(l):
-    if test_len(l):
-        if unique_ip(l):
+def unique_ns_list(l): # Takes in a list
+    if test_len(l):    # Tests if the length of the list is greater than 1
+        if unique_ip(l): # If that passes it checks for unique ip
             print("The servers are more than 1 and unique")
             return True
         else:
@@ -15,14 +15,16 @@ def unique_ns_list(l):
 def test_len(l):
     return len(l)>1
 
-def unique_ip(l):
-    y = socket.gethostbyname(l[0])
-    l2 = l[1:]
-    for x in l2:
+def unique_ip(l): 
+    y = socket.gethostbyname(l[0]) # Will return the ip adress of the host adress
+    l2 = l[1:] # creates a list starting from the second element
+    for x in l2: # "for each" loop for every element in the second list
         
         if y == socket.gethostbyname(x):
-            return False
+            return False # if the comparison ever detects same host ip it will return false
         elif len(l2)<2:
-            return True
+            return True # if the second list is ever lesser than 2 then it will return true
+ 
     
     return unique_ip(l2)
+ # uses recursive calls to return the answer
