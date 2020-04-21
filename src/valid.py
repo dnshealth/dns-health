d570bcd1a9c58b421ddda1a6065b83096479b587
Rudolfs Arvids Kalnins <rudolfsarvidskalnins@gmail.com>
2020-04-21 08:49:35 +0200

    Added description of program and formated comments

--- src/valid.py
+++ src/valid.py
@@ -1,12 +1,19 @@
 #Program checks if given hostname is valid
+#Takes hostname and list of nameservers as input
+#Returns False if hostname is not valid
+#Rturns True if hostname is valid
 import re
 
-def is_valid_hostname(hostname):
-  if len(hostname) > 255: #check if hostname correct length
+def is_valid_hostname(hostname, list_of_NS):
+  #check if hostname correct length
+  if len(hostname) > 255: 
     return False
   
   if hostname[-1] == ".":
-    hostname = hostname[:-1]  #strip end dot, if present
+    #strip end dot, if present
+    hostname = hostname[:-1]  
   
-  allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE) #regex to check for invalid simbols
-  return all(allowed.match(x) for x in hostname.split(".")) #return True if all parts of hostname are valid
+  #regex to check for invalid simbols
+  allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE) 
+  #return True if all parts of hostname are valid
+  return all(allowed.match(x) for x in hostname.split(".")) 
