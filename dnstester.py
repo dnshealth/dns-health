3d87afe5667e5510c3cfb429ad520346ffb77085
Byon <bagren@kth.se>
2020-04-21 14:29:27 +0200

    stuff

--- dnstester.py
+++ dnstester.py
@@ -1,13 +1,16 @@
-def check_recursive(ns_list):
+def check_recursive(q, ns_list):
     # checks for if RD flag is checked in the response
     import dns.message
     import dns.query
+    import socket
     recursion_exists = False
     for x in ns_list:
-        query = dns.message.make_query(x, dns.rdatatype.NS)
-        response = dns.message.make_response(query)
+        query = dns.message.make_query(q, dns.rdatatype.NS)
+        x = socket.gethostbyname(x)
+        x = "192.168.1.1"
+        response = dns.query.udp(query, x)
         s = str(response)
-        if "RD" in s:
+        if "RA" in s: # When "RA" is in the response message then the ns server tells the client that recursion have happened
             recursion_exists = True
-            print("The name server is set to use recursion when it tried to reach", x)
+            print("The name server is set to use recursion when it tried to query", x)
     return recursion_exists
