8cabbd4351dfffe9b48171a4542f70b1a50ae779
xyderos <konx_hd@hotmail.com>
2020-04-22 13:07:08 +0200

    DNSHEALTH-14: fixed issue with dns glue records

--- DNSChecker/getAuthoritativeServers.py
+++ DNSChecker/getAuthoritativeServers.py
@@ -1,22 +1,71 @@
 #!/usr/bin/env python3
 #DNSHEALTH-12
-import queryNameServers
+import dns.query, dns.resolver
+from dns.exception import DNSException
 
-#a function that returns all the authoritative servers for the given domain/domain
-#returns a list of tuples with what kind of record we received, the domain of the authority and the domain we queried
+# a functions that returns all the authoritative servers for the domain given.
+# Takes a domain as an argument and returns a tuple of the OK to indicate that it succeed 
+# and a list of tuple with all the authoritative domains.
 def getAuthoritativeServers(domain):
+
+    defaultResolver = dns.resolver.get_default_resolver()
     
-    (status,res) = queryNameServers.queryAuthoritativeNameServers(domain)
+    nameServers = defaultResolver.nameservers[0]
 
-    if status =="OK":
-   
-        result = []
+    arrayOfDomains = domain.split('.')
 
-        for i in res:
-            if i[0]=='AUTHORITATIVE':
-                result.append(i)
+    #a list that holds all the results for all the authoritative servers for a domain given
+    results = []
 
-        return result
+    #start by splitting our domain in all the possible subdomains
+    # eg testing.com.se would be split in .se, then in .com.se and finally in tesitng.com.se
+    for i in xrange(len(arrayOfDomains), 0, -1):
+        
+        subString = '.'.join(arrayOfDomains[i-1:])
 
-    else:
-        return "ERROR"
+        print('Looking up %s on %s' % (subString, nameServers))
+        
+        #make a query to send at the dns servers
+        query = dns.message.make_query(subString, dns.rdatatype.NS)
+        
+        #send the query for every name server we have
+        response = dns.query.udp(query, nameServers)
+
+        #retrieve the response code from the dns query we executed
+        responseCode = response.rcode()
+        
+        #An error occured(either we can't find the domain or something else happened) will be caught here and will probably exit
+        if responseCode != dns.rcode.NOERROR:
+            raise DNSException("AN ERROR OCCURED")
+        
+        #based onthe type of the reposnse, get a list of the responses of the rrsets
+        if len(response.authority) > 0:
+            rrsets = response.authority
+        elif len(response.additional) >0:
+            rrsets = response.additional
+        else:
+            rrsets = response.answer
+
+        #we must handle all the rrsets, not just the first one
+        #bases on the rdtype response, we need to find which name servers are authoritative
+        for rrset in rrsets:
+            for rr in rrset:
+
+                #when the same server is authoritative for the domain, pass
+                if rr.rdtype == dns.rdatatype.NS:
+                    
+                    nameServers = defaultResolver.query(rr.target).rrset[0].to_text()
+
+                    results.append(("AUTHORITATIVE",rr.target.to_text(),domain))
+
+                elif rr.rdtype == dns.rdatatype.SOA:
+                    results.append(("SAME AUTHORITATIVE SERVER", subString))
+
+                else:
+                    pass
+    return ("OK",results)
+
+(_,res)=getAuthoritativeServers("dnshealth.pw")
+
+for i in res:
+    print i
--- DNSChecker/getGlueRecords.py
+++ DNSChecker/getGlueRecords.py
@@ -1,22 +1,86 @@
 #!/usr/bin/env python3
 #DNSHEALTH-14
-import queryNameServers
+import dns
+import dns.resolver 
+import dns.query
+import dns.name
+from dns.exception import DNSException
 
-#a function that returns all the glue records for the given domain/domain
-#returns a list of tuples with what kind of record we received, the domain of the glue record and the domain we queried
+# a function that returns the glue records
+# takes the domain name 
+# returns a tuple w the ok sign and the list with the answers
 def getGlueRecords(domain):
+
+    nameServers         = []
+
+    name                = dns.name.from_text(domain)
+
+    defaultResolver     = dns.resolver.get_default_resolver()
+
+    depth               = 2
+
+    for nameServer in defaultResolver.nameservers:
     
-    (status,res) = queryNameServers.queryAuthoritativeNameServers(domain)
+            last = False
+    
+            while not last:
+                
+                subString    = name.split(depth)
+    
+                last         = subString[0].to_unicode() == '@'
+    
+                subString    = subString[1]
 
-    if status =="OK":
-   
-        result = []
+                query        = dns.message.make_query(subString, dns.rdatatype.NS)
+    
+                response     = dns.query.udp(query, nameServer)
+    
+                responseCode = response.rcode()
 
-        for i in res:
-            if i[0]=='GLUE':
-                result.append(i)
+                #if sth bad happens, thorw an exception
+                if responseCode != dns.rcode.NOERROR:
+                    raise DNSException("AN ERROR OCCURED")
+                
+                if len(response.authority) > 0:
+                    rrset = response.authority[0]
+                else:
+                    rrset = response.answer[0]
 
-        return result
+                #okay, we achieved to get a response and store it in the rrset
+                if len(rrset) > 0:
+                    done = True
 
-    else:
-        return "ERROR"
+                    for host in rrset:
+                    # we already have a list of nameservers for this domain, this is to get their IP addresses
+                        if host.rdtype == dns.rdatatype.SOA:
+                            print ('Same server is authoritative for {}'.format(sub))
+                        else:
+                            authority = host.target
+
+                            if depth == 3:
+
+                                ipv4 = defaultResolver.query(authority).rrset[0]
+
+                                ipv6 = None
+
+                                #might have an ipv6
+                                try:
+                                    ipv6 = defaultResolver.query(authority, rdtype=dns.rdatatype.AAAA).rrset[0]
+                                except DNSException as e:
+                                    pass
+                                
+                                nameServers += ("GLUE4",authority.to_text(),ipv4)
+
+                                nameServers += ("GLUE6",authority.to_text(),ipv6)
+                depth += 1
+            if done:
+                break
+
+    return ("OK",nameServers)
+
+
+(_,res) = getGlueRecords("dnshealth.pw")
+
+for i in res:
+    print i
+
