b529cf4b9c8a53e5e71172230640279a91d6529e
Kasper Oikarinen <kasper@sourceress.net>
2020-04-22 11:58:30 +0200

    Tests for network diversity

---
+++ src/tests/test_network_diversity.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from src.checks.network_diversity import run
+
+class Test(TestCase):
+    def test_run(self):
+        # This check should fail, as for some reason ns1.loopia.se and ns2.loopia.se have IPs within
+        # the same ASN.
+        # 93.188.0.20, 93.188.0.21
+        res = run("loopia.se", ["ns1.loopia.se", "ns2.loopia.se"])
+        assert not res["result"]
+
+    def test_run_2(self):
+        # This check should pass as Nordname obviously has their NS servers spread around the globe.
+        res = run("nordname.com", ["dns1.nordname.net", "dns2.nordname.net"])
+        assert res["result"]
