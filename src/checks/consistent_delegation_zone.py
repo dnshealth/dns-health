9b017acd4eaf20880b1e8e0fe694203cf81db5e8
Azad <azadm@kth.se>
2020-04-22 13:41:31 +0200

    minor formatting

--- src/checks/consistent_delegation_zone.py
+++ src/checks/consistent_delegation_zone.py
@@ -13,26 +13,26 @@ def run(hostname, list_of_NS):
     list_of_lists = []
     # Dns resolver initialization
     resolver = dns.resolver.Resolver()
-    
+
     # Getting nameserver IPs
     try:
         for x in list_of_NS:
             listNSIP.append(socket.gethostbyname(x))
     except socket.gaierror:
         return {"description": "IP for some nameserver could not be found", "result": False}
-    
+
     try:
-        # For every nameserver IP redefine the reslovers nameserver and query the hostname from that nameserver
+        # For every nameserver IP redefine the resolvers nameserver and query the hostname from that nameserver
         for name in listNSIP:
             resolver.nameservers = [name]
-            temp = []   
+            temp = []
             for data in resolver.query(hostname, 'NS'):
                 # Appending query results to a temporary list and removing end dot
                 temp.append(data.to_text()[:-1])
-            
+
             # Combining list of results from each query in to list of lists
             list_of_lists.append(sorted(temp))
-            
+
     # If query is refused return false
     except dns.resolver.NoNameservers:
         return {"description": "Some nameserver query failed", "result": False}
