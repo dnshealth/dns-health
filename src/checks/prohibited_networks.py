6f826d8f64a7b2fdbaa77adf42b1c89d39ef0723
hristog121 <hristog121@kth.se>
2020-04-23 09:34:17 +0200

    testing func -edit

--- src/checks/prohibited_networks.py
+++ src/checks/prohibited_networks.py
@@ -11,7 +11,8 @@
 import dns.resolver
 import ipaddress
 
-def run(domain,ns_list):
+
+def run(domain, ns_list):
     # Check each ns in ns_list. If one fails, immediately return false. Otherwise, return true after having checked everything
     for ns in ns_list:
         if not prohibited_check:
@@ -26,24 +27,20 @@ def prohibited_check(ns_server):
     shared_address_space = ipaddress.ip_network('100.64.0.0/10')
     result = dns.resolver.query(ns_server, 'A')
     for ipval in result:
-        if (ipaddress.ip_address(str(ipval)).is_private or
+        print(ipval)
+        if (
+                ipaddress.ip_address(str(ipval)).is_private or
                 ipaddress.ip_address(str(ipval)).is_multicast or
                 ipaddress.ip_address(str(ipval)).is_loopback or
                 ipaddress.ip_address(str(ipval)) in deprecated_ips or
                 ipaddress.ip_address(str(ipval)) in shared_address_space
         ):
-            print_fail()
             return False
 
         else:
-            print("[+] The IP for the for " + ns_server + " is: " + ipval.to_text())
-            print("[OK]")
             return True
 
 
-def print_fail():
-    print("[-] Error ! The IP is not a global IP.")
-    print("[-] TEST FAIL")
-
-
 # For debugging purposes please use prohibited_check("198.51.100.0")
+
+print(prohibited_check("ns1.loopia.se"))
