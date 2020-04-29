a9b726ea61e218633aac1f0675eab21a7eafb2d4
Rudolfs Arvids Kalnins <rudolfsarvidskalnins@gmail.com>
2020-04-29 08:53:27 +0200

    token function kinda working

--- src/api/server/swagger_server/controllers/authentication_controller.py
+++ src/api/server/swagger_server/controllers/authentication_controller.py
@@ -1,16 +1,22 @@
 import connexion
 import six
+import redis
+import uuid
 
 from swagger_server.models.token import Token  # noqa: E501
 from swagger_server import util
 
 
 def get_token():  # noqa: E501
-    """Send random token to user
+    r = redis.Redis()
+    token = uuid.uuid4().int
+    r.sadd("token:set", token)
+    return ({"token": token}, 200)
 
-    User is given token which is later used for authentication # noqa: E501
-
-
-    :rtype: Token
-    """
-    return 'do some magic!'
+def check_token(token):
+    r = redis.Redis()
+    if r.sismember("token:set", token):
+        return True
+    else:
+        return False
+    
