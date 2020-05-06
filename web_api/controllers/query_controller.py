728430c82dc00134cb76450fca94cba95f161faf
Rudolfs Arvids Kalnins <rudolfsarvidskalnins@gmail.com>
2020-05-06 10:46:11 +0200

    captcha verifying initial commit

--- web_api/controllers/query_controller.py
+++ web_api/controllers/query_controller.py
@@ -11,11 +11,13 @@ import dns.name
 import redis
 from datetime import datetime
 import itertools
+import json
 
 from web_api.models.check import Check  # noqa: E501
 from web_api.models.inv_par import InvPar  # noqa: E501
 from web_api.models.result import Result  # noqa: E501
 from web_api import util
+from urllib.request import urlopen
 
 TIME_LIMIT = 5
 
@@ -29,32 +31,39 @@ def test_servers(body):  # noqa: E501
 
     :rtype:(dictionary, int)
     """
-    #Converts request to object of type Check
+    # Converts request to object of type Check
     if connexion.request.is_json:
         body = Check.from_dict(connexion.request.get_json())  # noqa: E501
 
-    #Extract the domain string and name server list and token string from the Check object
+    # Extract the domain string and name server list and token string and recpatcha response from the Check object
     domain = body.domain
     name_servers = body.nameservers
     token = body.token
+    captcha_respone = body.g-recaptcha-response
+    
+    if captcha_respone == None or captcha_respone == "":
+        return  ({"errorDesc": "No token given!"}, 400)
+    
+    if not verify_captcha(captcha_respone):
+        return  ({"errorDesc": "reCaptcha verification failed"}, 400)
 
-    #If the field are empty. return an error
+    # If the field are empty. return an error
     if domain == "" or domain == None or name_servers == [] or name_servers == None or name_servers == [None]:
         return ({"errorDesc": "One of the fields is empty!"}, 400)
 
-    #If the user entered a non valid hostname, stop and don't run the other tests
+    # If the user entered a non valid hostname, stop and don't run the other tests
     if not checks.valid_hostname.run(domain, name_servers).get("result"):
         return ({"errorDesc": "Wrong hostname format"}, 400)
    
-    #Checks if a token has been provided
+    # Checks if a token has been provided
     if token == None or token == "":
         return ({"errorDesc": "No token given!"}, 400)
     
-    #Validates token
+    # Validates token
     if not check_token(token):
         return ({"errorDesc": "Invalid token!"}, 400)
 
-    #Limits the rate at which the user may query the database 
+    # Limits the rate at which the user may query the database 
     if not check_time_limit(token):
         return ({"errorDesc": "Too many queries in {0} seconds!".format(TIME_LIMIT)}, 400)
         
@@ -81,7 +90,7 @@ def test_servers(body):  # noqa: E501
             result = {"result": result, "description": str(check.__name__)}
         results.append(result)
 
-    #Gives each check result a unique id and parses and combines them into the correct format
+    # Gives each check result a unique id and parses and combines them into the correct format
     check_id = 0
     list_of_check_results = []
     for outcome in results:
@@ -89,7 +98,7 @@ def test_servers(body):  # noqa: E501
         list_of_check_results.append({"id":check_id, "result": outcome.get("result"), "key": details})
         check_id += 1
 
-    #Creates the necessary JSON response as a dictionary
+    # Creates the necessary JSON response as a dictionary
     response = {"domain": domain, "ns": name_servers, "checks":list_of_check_results}
 
     #Return the results of the checks and send the 200 OK code
@@ -101,6 +110,29 @@ conn_params = {
     "password": None,
     "db": 0
 }
+
+# This is a test key currently TODO change this to the real key
+RECAPTCHA_SECRET_KEY = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
+
+def verify_captcha(response):
+    # Creating a url for POST request
+    # Google's recaptcha verification api
+    url = "https://www.google.com/recaptcha/api/siteverify?"
+    
+    # Captcha secret key
+    url += 'secret=' + str(RECAPTCHA_SECRET_KEY) + '&'
+    
+    # g-recpatcha-response sent from front end
+    url += 'response=' + str(response)
+    
+    # Read response from the crafted url
+    json_obj = json.loads(urlopen(url).read())
+    
+    # json_obj is a dictionary with a boolean 'success' field, thus check if it is true or false
+    if json_obj['success']:
+        return True
+    else:
+        return False
 
 # Check if token given by client is valid
 def check_token(token):
@@ -116,7 +148,7 @@ def check_token(token):
 
 def check_time_limit(token):
 
-    #init a client instance for redis
+    # init a client instance for redis
     r = redis.Redis(**conn_params)
 
     time = r.hget("token_hash", token)
@@ -131,9 +163,10 @@ def check_time_limit(token):
         return False
     
     else:
-        #lazy update the set
+        # lazy update the set
         r.hdel("token_hash", token)
         
         r.hset("token_hash", token, datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)"))
         
         return True
+    
