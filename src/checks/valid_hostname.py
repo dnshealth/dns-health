315af188884e338e3ca013ea5002d8872c965d29
Rudolfs Arvids Kalnins <rudolfsarvidskalnins@gmail.com>
2020-05-12 16:03:01 +0200

    added more details in validhost

--- src/checks/valid_hostname.py
+++ src/checks/valid_hostname.py
@@ -7,17 +7,16 @@ import re
 
 
 def run(hostname, list_of_NS):
-    description = "Valid hostnames"
+    description = "Valid hostname"
 
     # Check if hostname correct length
     if len(hostname) > 255:
         return {"description": description, "result": False}
-
-    if hostname[-1] == ".":
-        # Strip end dot, if present
+    
+    # Strip end dot, if present
+    if hostname[-1] == ".":    
         hostname = hostname[:-1]
         
-
 
     # Strip end dots from nameservers
     for i in range(len(list_of_NS)):
@@ -28,17 +27,32 @@ def run(hostname, list_of_NS):
     # Regex to check for invalid simbols
     allowed = re.compile(r"(?!-)[a-z0-9-]{1,63}(?<!-)$", re.IGNORECASE)
     
-    # Return True if all parts of hostname are valid
+    # Check if hostname has correct number of parts
     if len(hostname.split(".")) < 2:
-        return {"description": description, "result": False}
+        return {"description": description, "result": False, "details": "Hostname incorrect form"}
     
+    # Return True if all parts of hostname are valid
     result = all(allowed.match(x) for x in hostname.split("."))
     
-    # Also check that the nameservers have correct format
+    # Check if all nameservers, have correct number of parts
     for x in list_of_NS:
         if len(x.split(".")) < 3:
-            return {"description": description, "result": False}
+            return {"description": description, "result": False, "details": "Nameserver(s) incorrect form"}
         
-    result &= all(allowed.match(y) for x in list_of_NS for y in x.split("."))
-
-    return {"description": description, "result": result}
+    # Return True if all parts of nameserver are valid 
+    resultNS = all(allowed.match(y) for x in list_of_NS for y in x.split("."))
+        
+    # If both domain and nameserver addresses are valid return True otherwise False
+    if result and resultNS:
+        return {"description": description, "result": True, "details": ""}
+    
+    elif result and not resultNS:
+        return {"description": description, "result": False, "details": "Nameserver(s) are invalid"}
+    
+    elif not result and resultNS:
+        return {"description": description, "result": False, "details": "Hostname is invalid"}
+    
+    else:
+        return {"description": description, "result": False, "details": "Hostname and nameservers are invalid"}
+    
+print(run("aaa.aaa", ["aa.aaa.aaa"]))
